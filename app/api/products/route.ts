537dfab10f9d88d29e85aed930c1bd5d68b38fd3
Cory House <housecor@gmail.com>
2023-08-23 15:28:31 -0500

    add sleep

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,6 +1,8 @@
 import { mockProducts } from "@/mocks/products.mocks";
+import { sleep } from "@/utils/sleep";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
+  await sleep(2000);
   return NextResponse.json(mockProducts);
 }
