49ad63957a44a1a5ab04d4cfce15af2d81a24a89
Cory House <housecor@gmail.com>
2023-08-24 14:30:50 -0500

    add

--- app/@homeBanner2/page.tsx
+++ app/@homeBanner2/page.tsx
@@ -1,3 +1,6 @@
-export default function HomeBanner2() {
+import { sleep } from "@/utils/sleep";
+
+export default async function HomeBanner2() {
+  await sleep(2000);
   return <h2>Home Banner 2</h2>;
 }
