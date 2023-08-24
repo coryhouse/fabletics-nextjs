6a791e53a5adcb607e03e89dab924a17c0e7a270
Cory House <housecor@gmail.com>
2023-08-24 14:57:24 -0500

    add

---
+++ app/cart/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useCartContext } from "@/context/cart-context";
+
+export default function CartPage() {
+  const { cart } = useCartContext();
+
+  return (
+    <>
+      <h1>Cart</h1>
+      <ul>
+        {cart.map((productId) => {
+          return <li key={productId}>{productId}</li>;
+        })}
+      </ul>
+    </>
+  );
+}
