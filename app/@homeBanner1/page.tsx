9f932f86010f1aa5f65631b5ccea4000bd92cd04
Cory House <housecor@gmail.com>
2023-08-24 14:03:08 -0500

    add

---
+++ app/@homeBanner1/page.tsx
@@ -0,0 +1,7 @@
+export default function HomeBanner1() {
+  return (
+    <div style={{ width: "100%", backgroundColor: "blue" }}>
+      <h1>Home Banner 1</h1>
+    </div>
+  );
+}
---
+++ app/@homeBanner2/page.tsx
@@ -0,0 +1,3 @@
+export default function HomeBanner2() {
+  return <h2>Home Banner 2</h2>;
+}
