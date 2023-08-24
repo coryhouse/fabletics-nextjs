a64fbf318f630a31504b8718c68575630af326eb
Cory House <housecor@gmail.com>
2023-08-24 14:26:15 -0500

    add

---
+++ app/@homeBanner1/error.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function Error() {
+  return <h1>Home banner 1 failed to load.</h1>;
+}
--- app/@homeBanner1/page.tsx
+++ app/@homeBanner1/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+
+import { useState } from "react";
+
 export default function HomeBanner1() {
+  const [showError, setShowError] = useState(false);
+
+  if (showError) throw new Error("Home banner 1 failed to load.");
+
   return (
     <div style={{ width: "100%", backgroundColor: "blue" }}>
       <h1>Home Banner 1</h1>
+      <button onClick={() => setShowError(true)}>Throw error</button>
     </div>
   );
 }
