80889a9ee37fbddf1eec5d619e22a7d262ea647b
Cory House <housecor@gmail.com>
2023-08-24 15:16:33 -0500

    add

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,10 +1,14 @@
+"use client";
+import { useCartContext } from "@/context/cart-context";
 import NavLink from "./nav-link";
 
 export default function Nav() {
+  const { cart } = useCartContext();
   return (
     <nav>
       <NavLink path="/">Home</NavLink> |{" "}
-      <NavLink path="/products">Products</NavLink>
+      <NavLink path="/products">Products</NavLink> |{" "}
+      <NavLink path="/cart">Cart ({cart.length})</NavLink>
     </nav>
   );
 }
